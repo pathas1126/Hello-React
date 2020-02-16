a19e409f8ae42e1e8f1d89ec7372906fb738d8bb
pathas <pathas@naver.com>
2020-02-16 23:26:32 +0900

    Practice State & Props with EventHandler

--- react-project/src/components/PhoneItem/PhoneItem.js
+++ react-project/src/components/PhoneItem/PhoneItem.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "./PhoneItem.module.scss";
 
-const PhoneItem = () => {
+const PhoneItem = ({ id, name, phone, onClick }) => {
   return (
     <div className={styles.phone_item}>
       <div className={styles.phone_item_left}>
-        <div className={styles.phone_item_name}>홍길동</div>
-        <div className={styles.phone_item_phone}>010-2222-4444</div>
+        <div className={styles.phone_item_name}>{name}</div>
+        <div className={styles.phone_item_phone}>{phone}</div>
       </div>
       <div className={styles.phone_item_rightg}>
-        <button>삭제</button>
+        <button onClick={() => onClick(id)}>삭제</button>
       </div>
     </div>
   );
---
+++ react-project/src/lib/dummyData.js
@@ -0,0 +1,43 @@
+export const dummyData = {
+  "0": {
+    id: "0",
+    name: "John",
+    phone: "010-0000-0000"
+  },
+  "1": {
+    id: "1",
+    name: "Peter",
+    phone: "010-1111-1111"
+  },
+  "2": {
+    id: "2",
+    name: "Elise",
+    phone: "010-2222-2222"
+  },
+  "3": {
+    id: "3",
+    name: "Chris",
+    phone: "010-3333-3333"
+  },
+  "4": {
+    id: "4",
+    name: "Austin",
+    phone: "010-4444-4444"
+  },
+  "5": {
+    id: "5",
+    name: "Adam",
+    phone: "010-5555-5555"
+  },
+  "6": {
+    id: "6",
+    name: "HolyMoly",
+    phone: "010-6666-6666"
+  }
+};
+
+export var nextId = Object.keys(dummyData).length;
+
+export const setNextId = () => {
+  nextId++;
+};
